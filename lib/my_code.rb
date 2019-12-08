409e193c7b5f05d268d3f8f51e94f8c1ff72c22b
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-08 17:22:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,4 +10,12 @@ def map(array)
 end
 
 def reduce(array, starting_point = 0)
-  
+  new = starting_point
+  i = 0
+
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
+end
